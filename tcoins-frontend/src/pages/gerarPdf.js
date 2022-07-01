24f801589c732164f747f4ccd91799d4a2cec428
joao.souza <joao.souza@usto.re>
2022-07-01 00:27:00 -0300

    relatorio nilson

--- tcoins-frontend/src/pages/gerarPdf.js
+++ tcoins-frontend/src/pages/gerarPdf.js
@@ -1,10 +1,51 @@
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
 
+function gerar(tabela){
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
+    const reportTitle = [
+       {
+        text: 'Relatório em PDF',
+        fontSize: 15,
+        bold:true,
+        margin:[15, 20, 0, 45],
+        
+    }];
+    const dados = tabela.map((dado)=>{
+        return [
+            {text:dado.id, fontSize:10},
+            {text:dado.nome, fontSize:10}
+        ]
+    })
+    const details = [
+        {
+            table:{
+                headerRows: 1,
+                body:[
+                    [
+                        {text:'id', style:'tableHeader', fontSize:12},
+                        {text:'Nome', style:'tableHeader', fontSize:12}
+                    ],
+                    ...dados
+                ]
+            },
+            layout: 'lightHorizontalLines'
+        }
+    ];
+    const rodape = [];
 
-function GerarPdf(){
-    
+    const docDefinition = {
+        pageSize: 'A4',
+        pageMargins: [15, 50,15,40],
+
+        header: [reportTitle],
+        content: [details],
+        footer: [rodape]
+    }
+    pdfMake.createPdf(docDefinition).download();
 }
-export default GerarPdf();
+export default gerar;
 
 
 
