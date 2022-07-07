2b40575fc7202fb5966b3f3e27de12e4215973cc
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-07-07 00:32:40 -0300

    Criação do service de loja no react

--- tcoins-frontend/src/services/LojaService.js
+++ tcoins-frontend/src/services/LojaService.js
@@ -5,6 +5,21 @@ const LojaService = {
     getLojas(nomeLoja = "", ramoLoja = "", currentpage = "", pageSize = ""){
         const header = { nomeLoja, ramoLoja }
         RequisicaoService.get(`loja?currentpage=${currentpage}&pageSize=${pageSize}`, header);
+    },
+
+/*     postLojas(nomeLoja = "", ramoLoja = ""){
+        const header = { nomeLoja, ramoLoja }
+        RequisicaoService.post( "loja", header);
+    } */
+
+    postLojas(donoId = "", ramoId = "", nome = "",descricao = "", latitude = "", longitude = "", imagem = ""){
+        const header = { donoId, ramoId, nome, descricao, latitude, longitude, imagem }
+        RequisicaoService.post( "loja", header);
+    },
+
+    deleteLojas(lojaId = "") {
+        const header = { lojaId }
+        RequisicaoService.delete( "loja", header);
     }
 }
 
