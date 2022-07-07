bf9b36fa109fc65b1b541ff42241cbef0935f3ba
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-07-07 00:31:11 -0300

    Chamando o service de loja na tela inicial

--- tcoins-frontend/src/pages/home/Home.js
+++ tcoins-frontend/src/pages/home/Home.js
@@ -1,24 +1,66 @@
 import React from 'react';
+import LojaService from '../../services/LojaService';
 import MultiActionAreaCard from '../../components/MultiActionAreaCard';
+import BarraPesquisa from '../../components/BarraPesquisa';
+import { useState, useEffect } from 'react';
 
 function Home() {
 
+    /* const api = axios.create({
+        baseURL: 'https://servicodados.ibge.gov.br/api/v1/localidades/'
+    }); */
+    const [getdata, setData] = useState([]);
+
+
+    function resgatarDados() {
+        LojaService.getLojas('nomeLoja')
+            .then(function (response) {
+                setData(response.data);
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        resgatarDados()
+    }, [])
+
     return (
+
         <div className="App">
+            <div className='ContainerHome'>
+                <BarraPesquisa api={LojaService} setData={setData} resgatarDados={resgatarDados}></BarraPesquisa>
 
-            <div className="Card">
-                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
-                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
-                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+                <div className="Card">
+                    {getdata.map((dado, i) => (
+                        <MultiActionAreaCard data={dado} className="CardItem"></MultiActionAreaCard>
+
+                    ))}
+                </div>
+
+
             </div>
 
-            <div className="Card">
-                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
-                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
-                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
-            </div>
 
         </div>
+
+/*         <div className="App">
+
+            <div className="Card">
+                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+            </div>
+
+            <div className="Card">
+                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+                <MultiActionAreaCard className="CardItem"></MultiActionAreaCard>
+            </div>
+
+        </div> */
     )
 }
 /*API: https://tcoinsapp.herokuapp.com/api/tcoins*/
