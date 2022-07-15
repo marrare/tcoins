4905947d52ab2f5b413bf8212437769daa7636d7
roberta976 <robertavitoria976@gmail.com>
2022-07-14 23:10:38 -0300

    ajustando a requisição de dados

--- tcoins-frontend/src/pages/home/Home.js
+++ tcoins-frontend/src/pages/home/Home.js
@@ -1,46 +1,48 @@
 import React from 'react';
-import MultiActionAreaCard from '../components/MultiActionAreaCard';
-import BarraPesquisa from '../components/BarraPesquisa'
+import MultiActionAreaCard from '../../components/MultiActionAreaCard';
+import BarraPesquisa from '../../components/BarraPesquisa'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import LojaService from '../../services/LojaService';
+import Pagination from '@mui/material/Pagination';
 
-function Home() {
-    const api = axios.create({
-        baseURL: 'https://servicodados.ibge.gov.br/api/v1/localidades/'
-    });
-    const [getdata, setData] = useState([]);
-
-
-    function resgatarDados() {
-        api.get('estados/PE/distritos')
-            .then(function (response) {
-                setData(response.data);
-                // console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+function Home({ props }) {
+    const [page, setPage] = React.useState(1);
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
+    const [lojasHome, setLojas] = useState([]);
 
     useEffect(() => {
-        resgatarDados()
-    }, [])
+        getLojas();
+        // console.log(page)
+        // console.log(getdata)
+    }, [page])
+
+    async function getLojas() {
+        const lojas = await LojaService.getLojas('', '', page, 4);
+        if (lojas.status == 200 || lojas.status == 404) setLojas(lojas.data);
+    }
 
 
     return (
         <div className="App">
             <div className='ContainerHome'>
-                <BarraPesquisa setData={setData} api={api}></BarraPesquisa>
+                <BarraPesquisa setLojas={setLojas}></BarraPesquisa>
 
-            <div className="Card">
-                {getdata.map((dado, i) => (
-                    <MultiActionAreaCard data={dado} key={i} className="CardItem"></MultiActionAreaCard>
+                <div className="Card">
+                    {lojasHome.map((dado, i) => (
+                        <MultiActionAreaCard loja={dado} key={i} className="CardItem"></MultiActionAreaCard>
 
-                ))}
+                    ))}
+                </div>
+                <div className='Paginacao'>
+                    <Pagination color="secondary" count={10} page={page} onChange={handleChange} />
+                </div>
+                
+
+
             </div>
-
-
-        </div>
 
 
         </div >
