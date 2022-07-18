503abf46f9787f169881dfb12ac4a31a645bd369
allesson <allessonqueiroz@gmail.com>
2022-07-18 18:24:56 -0300

    Controller e service de RAMOS

--- tcoins-backend/src/main/java/br/ifpe/tcoins/service/RamoService.java
+++ tcoins-backend/src/main/java/br/ifpe/tcoins/service/RamoService.java
@@ -5,6 +5,8 @@ import br.ifpe.tcoins.repository.LojaRamosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RamoService {
     @Autowired
@@ -13,5 +15,9 @@ public class RamoService {
 
     public LojaRamos getRamoById(Long ramoId) {
         return ramosRepository.getById(ramoId);
+    }
+
+    public List<LojaRamos> getAll() {
+        return  ramosRepository.findAll();
     }
 }
