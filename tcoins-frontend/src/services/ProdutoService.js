a513bf7f9056d6e72ca70fe27fbf06050cd6451c
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-07-19 23:47:04 -0300

    Criação do service de produto no frontend

--- tcoins-frontend/src/services/ProdutoService.js
+++ tcoins-frontend/src/services/ProdutoService.js
@@ -2,6 +2,13 @@ import RequisicaoService from "./RequisicaoService";
 
 const ProdutoService = {
 
+    async getProdutos(page = "", pageSize = "") {
+        const header = { page, pageSize}
+        const ret = await RequisicaoService.get(`loja?page=${page}&pageSize=${pageSize}`, header);
+        return ret;
+
+    }
+
 }
 
 export default ProdutoService;
