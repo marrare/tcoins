a813d91f77823d448a0704ba5adfa73d8e43a292
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-07-22 00:12:59 -0300

    Criação do service de loja no frontend

--- tcoins-frontend/src/services/LojaService.js
+++ tcoins-frontend/src/services/LojaService.js
@@ -6,6 +6,26 @@ const LojaService = {
         const header = { nomeLoja, ramoLoja }
         const ret = await RequisicaoService.get(`loja?currentPage=${currentpage}&pageSize=${pageSize}`, header);
         return ret;
+    },
+
+    async cadastrarLojas(userId, ramoId, nome, descricao, latitude, longitude, imagem) {
+        const header = { userId, ramoId}
+        const ret = await RequisicaoService.post("/loja", {
+            nome: nome,
+            descricao: descricao,
+            latitude: latitude,
+            longitude: longitude,
+            imagem: imagem
+        }, header)
+        return ret;
+
+    },
+
+    async deletarojas(lojaId) {
+        const header = { lojaId }
+        const ret = await RequisicaoService.delete("/loja", header)
+        return ret;
+
     }
 }
 
