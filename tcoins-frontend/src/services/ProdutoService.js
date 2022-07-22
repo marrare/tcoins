5e2ffc28db96ace9c7ee0c45bd46bb7ca6f097aa
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-07-22 00:14:22 -0300

    Criação do service de produto no frontend

--- tcoins-frontend/src/services/ProdutoService.js
+++ tcoins-frontend/src/services/ProdutoService.js
@@ -2,9 +2,41 @@ import RequisicaoService from "./RequisicaoService";
 
 const ProdutoService = {
 
-    async getProdutos(page = "", pageSize = "") {
-        const header = { page, pageSize}
-        const ret = await RequisicaoService.get(`loja?page=${page}&pageSize=${pageSize}`, header);
+    async getProdutos(lojaId, page = "", pageSize = "") {
+        const header = { lojaId, page, pageSize}
+        const ret = await RequisicaoService.get("/produto/loja", header, {});
+        return ret;
+
+    },
+
+    async cadastrarProdutos(lojaId, nome, descricao, precoTcoins, valorRecompensa, imagem) {
+        const header = { lojaId }
+        const ret = await RequisicaoService.post("/produto", {
+            nome: nome,
+            descricao: descricao,
+            precoTcoins: precoTcoins,
+            valorRecompensa: valorRecompensa,
+            imagem: imagem
+
+        }, header)
+        return ret;
+    },
+
+    async deletarProdutos(id) {
+        const header = { id }
+        const ret = await RequisicaoService.delete("/produto", header)
+        return ret;
+    },
+
+    async atualizarProdutos(produtoId, lojaId, nome, descricao, precoTcoins, valorRecompensa, imagem) {
+        const header = { produtoId, lojaId }
+        const ret = await RequisicaoService.update("/produto", {
+            nome: nome,
+            descricao: descricao,
+            precoTcoins: precoTcoins,
+            valorRecompensa: valorRecompensa,
+            imagem: imagem
+        }, header)
         return ret;
 
     }
