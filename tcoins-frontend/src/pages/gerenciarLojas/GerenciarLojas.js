9043d289b0ef5991b9a5b335b75e607d6aa3e236
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-07-30 01:38:30 -0300

    Criação do modal para cadastrar lojas

--- tcoins-frontend/src/pages/gerenciarLojas/GerenciarLojas.js
+++ tcoins-frontend/src/pages/gerenciarLojas/GerenciarLojas.js
@@ -15,6 +15,27 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+
+};
+
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -56,7 +77,20 @@ const rows = [
 ];
 
 export default function CustomizedTables() {
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const [ramo, setRamo] = React.useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setRamo(event.target.value);
+    };
+
+
     return (
+
 
         <div className="Corpo">
             <div className="Barra">
@@ -74,21 +108,95 @@ export default function CustomizedTables() {
                         <SearchIcon />
                     </IconButton>
                     <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-                    <IconButton color="primary" sx={{ p: '10px' }} aria-label="add">
+                    <IconButton color="primary" sx={{ p: '10px' }} aria-label="add" onClick={handleOpen}>
                         <AddIcon />
                     </IconButton>
                 </Paper>
             </div>
 
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ textAlign: 'center' }}>
+                        Adicionar Loja
+                    </Typography>
+                    <TextField sx={{ marginTop: 2, marginBottom: 1 }}
+                        autoComplete="given-name"
+                        name="Nome"
+                        required
+                        fullWidth
+                        id="firstName"
+                        label="Nome"
+                        autoFocus
+                    />
+                    <InputLabel id="demo-simple-select-label">Ramo</InputLabel>
+                    <Select sx={{ marginBottom: 1 }}
+                        autoComplete="given-name"
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        name="Ramo"
+                        required
+                        fullWidth
+                        value={ramo}
+                        label="Ramo"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                    <InputLabel id="demo-simple-select-label">Foto</InputLabel>
+                    <TextField sx={{marginBottom: 1 }}
+                        name="upload-photo"
+                        type="file"
+                        fullWidth
+                    />
+                    <TextField
+                        sx={{ height: 8, marginBottom: 5 }}
+                        autoComplete="given-name"
+                        name="Descricao"
+                        required
+                        fullWidth
+                        id="Descricao"
+                        label="Descrição"
+                    />
+                    <Button
+                        sx={{ marginBottom: 1, backgroundColor: 'red', marginTop: 2 }}
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        onClick={handleClose}
+                    >
+                        CANCELAR
+                    </Button>
+                    <Button
+                        sx={{ backgroundColor: 'green' }}
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                    >
+                        CONFIRMAR
+                    </Button>
+                </Box>
+            </Modal>
+
+
             <div classsName="Tabela">
-                <TableContainer component={Paper} sx={{ maxWidth: 1000, margin: 'auto' }} >
+                <TableContainer component={Paper} sx={{ maxWidth: 1000, margin: 'auto', marginBottom: 5 }} >
                     <Table aria-label="customized table">
                         <TableHead>
                             <TableRow>
                                 <StyledTableCell align="left" sx={{ fontSize: '1.1rem' }}>Imagem</StyledTableCell>
-                                <StyledTableCell align="left" sx={{ fontSize: '1.1rem'}}>Nome</StyledTableCell>
-                                <StyledTableCell align="left" sx={{ fontSize: '1.1rem'}}>Ramo</StyledTableCell>
-                                <StyledTableCell align="center" sx={{ fontSize: '1.1rem'}}>Ações</StyledTableCell>
+                                <StyledTableCell align="left" sx={{ fontSize: '1.1rem' }}>Nome</StyledTableCell>
+                                <StyledTableCell align="left" sx={{ fontSize: '1.1rem' }}>Ramo</StyledTableCell>
+                                <StyledTableCell align="center" sx={{ fontSize: '1.1rem' }}>Ações</StyledTableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
