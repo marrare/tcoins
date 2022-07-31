f37150da388fb69b6789346ab323c47184924809
roberta976 <robertavitoria976@gmail.com>
2022-07-30 23:11:21 -0300

    passando o nome da loja na rota

--- tcoins-frontend/src/App.js
+++ tcoins-frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/gerenciar-lojas" element={<GerenciarLojas />} />
-        <Route path="/gerenciar-produtos" component={MultiActionAreaCard} element={<GerenciarProdutos />} />
+        <Route path="/loja/:nome" element={<GerenciarProdutos />} />
       </Routes>
 
       <Footer></Footer>
