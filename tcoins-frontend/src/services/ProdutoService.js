7a0d6c7d82326f3a6a849f3a21e2e9da74da52c5
roberta976 <robertavitoria976@gmail.com>
2022-08-05 11:08:37 -0300

    service de produto ajustado

--- tcoins-frontend/src/services/ProdutoService.js
+++ tcoins-frontend/src/services/ProdutoService.js
@@ -2,13 +2,28 @@ import RequisicaoService from "./RequisicaoService";
 
 const ProdutoService = {
 
-    async getProdutosPorLoja(lojaId , page = "", pageSize = "") {
-        const header = { lojaId, page, pageSize}
-        const ret = await RequisicaoService.get(`/produto/loja`, header);
+    async getProdutosByLoja(nomeProduto = "", lojaId = "", currentpage = "", pageSize = "") {
+        const header = { nomeProduto, lojaId }
+        const ret = await RequisicaoService.get(`produto?currentPage=${currentpage}&pageSize=${pageSize}`, header);
         return ret;
-
-    }
-
+    },
+    async createProduto(lojaId = "", produto) {
+        const header = {lojaId }
+        const body = {produto}
+        const ret = await RequisicaoService.post('produto', body, header);
+        return ret;
+    },
+    async updateProduto(produtoId = "", lojaId = "", produto) {
+        const header = { produtoId, lojaId }
+        const body = {produto}
+        const ret = await RequisicaoService.update('produto', body, header);
+        return ret;
+    },
+    async deleteProduto(id) {
+        const header = {id}
+        const ret = await RequisicaoService.delete('produto', header);
+        return ret;
+    },
 }
 
 export default ProdutoService;
