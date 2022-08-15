e104cf47830236bd3e70caf7c24fba33b8901952
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-08-15 18:17:06 -0300

    Criação do campo de plano vigente

--- tcoins-frontend/src/pages/planos/Planos.js
+++ tcoins-frontend/src/pages/planos/Planos.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import './Planos.css'
+
+import Button from '@mui/material/Button';
 
 function Planos() {
 
-    return(
+    return (
+
         <div>
-            Planos
+            <div className="planoAtual">
+                <div className="titulo">
+                    <h3>Plano Vigente</h3>
+                </div>
+                <div className='containerDetalhePlano'>
+                    <div className="detalhePlano">
+                        <p>Nome do Plano: </p>
+                        <p>Data de Contratação: </p>
+                        <p>Data de Expiração: </p>
+                    </div>
+                    <div className='botaoPlano'>
+                    <Button
+                        sx={{ marginBottom: 1 }}
+                        className='botao'
+                        type="submit"
+                        color="success"
+                        fullWidth
+                        variant="contained"
+                    >
+                        Trocar de Plano
+                    </Button>
+                    <Button
+                        type="submit"
+                        color="error"
+                        fullWidth
+                        variant="outlined"
+                    >
+                        Cancelar Plano
+                    </Button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
