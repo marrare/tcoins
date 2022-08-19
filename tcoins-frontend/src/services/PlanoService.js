fbe4d419ca580a4c7e672584d3826c8f0c0ef8de
Victor Guthierry Dias da Silva <victor.guthierry.dias@gmail.com>
2022-08-18 22:48:50 -0300

    Criando service de planos no frontend

---
+++ tcoins-frontend/src/services/PlanoService.js
@@ -0,0 +1,14 @@
+import RequisicaoService from "./RequisicaoService";
+
+const PlanoService = {
+
+    async atualizarPlano(userId="", planoId="") {
+        const header = { "userId":userId, "planoId":planoId, "duracao":1 }
+        const ret = await RequisicaoService.update('plano', header);
+        return ret;
+
+    }
+
+}
+
+export default PlanoService;
