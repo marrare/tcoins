596a5cff45ed325076206364d86b0ded23b9d4db
joao.souza <joao.souza@usto.re>
2022-08-19 02:49:58 -0300

    service de plano

--- tcoins-frontend/src/services/PlanoService.js
+++ tcoins-frontend/src/services/PlanoService.js
@@ -4,7 +4,7 @@ const PlanoService = {
 
     async atualizarPlano(userId="", planoId="") {
         const header = { "userId":userId, "planoId":planoId, "duracao":1 }
-        const ret = await RequisicaoService.update('plano', header);
+        const ret = await RequisicaoService.update('plano', {}, header);
         return ret;
 
     }
